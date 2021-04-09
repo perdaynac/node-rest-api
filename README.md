a15c4883ff6199a2d7ca08bf088f5fc8877c18e3
Per Daynac <hello@perdaynac.com>
2021-04-09 16:06:04 +0200

    Adds https with OpenSSL and some basic routing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-rest-api
+
+
+# OpenSSL
+Needs a https folder in the root directory containing OpenSSL files cert.pem and key.pem.
+To add these files install OpenSSL (if not already installed) and run this following command in the https directory:
+> openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
--- index.js
+++ index.js
@@ -7,41 +7,161 @@
 
 // Dependencies
 const http = require('http');
+const https = require('https');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
 
 
+// Instantiating HTTP server
+const httpServer = http.createServer(function(req, res) {
 
-// The server should respond to all request with a string
-const server = http.createServer(function(req, res) {
+    unifiedServer(req, res);
+  
 
-  // Get Url and parse it
-  var parsedUrl = url.parse(req.url, true);
+});
 
-  // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+// Start the server and have it listen to the port specified in config-file/console
+httpServer.listen(config.httpPort, function() {
 
-
-  // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-
-  // Get HTTP method
-  var method = req.method.toLowerCase();
-
-  // Send the response
-  res.end('Hello world!\n');
-
-  // Log the request path
-  console.log('Request receive on path: ' + trimmedPath + ' with this method: ' + method + ' with these query string parameters: ', queryStringObject);
+  console.log("The server is listening to port " + config.httpPort + " in " + config.envName + " mode");
 
 
 });
 
-// Start the server and have it listen to port 3000
-server.listen(3000, function() {
+// Instantiating HTTP server
+let httpsServerOptions = {
+    'key' : fs.readFileSync('./https/key.pem'),
+    'cert' : fs.readFileSync('./https/cert.pem')
+};
 
-  console.log("The server is listening to port 3000 now");
+const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+
+    unifiedServer(req, res);
+  
+
+});
+
+// Start the server and have it listen to the port specified in config-file/console
+httpsServer.listen(config.httpsPort, function() {
+
+  console.log("The server is listening to port " + config.httpsPort + " in " + config.envName + " mode");
 
 
 });
+
+// All the server logic for both the http and https server
+let unifiedServer = function (req, res) {
+
+    // Get Url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+
+    // Get HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Set the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+    
+        buffer += decoder.write(data);
+
+    });
+
+    req.on('end', function(){
+    
+        buffer += decoder.end();
+
+        // Choose the handler this request should go to
+        // If one is not found, use the notFound handler
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Construct the data object to send to the handler
+        var data = {
+
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer,
+
+
+        };
+
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode, payload) {
+
+            // Use the statusCode called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+
+            // Convert the payload to a string
+            payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+            //console.log('Request receive on path: ' + trimmedPath + ' with this method: ' + method + ' with these query string parameters: ', queryStringObject);
+            //console.log('Request received with this payload: ', buffer);
+
+        }); 
+
+    });
+
+
+}
+
+
+
+
+// Define the handlers
+var handlers = {};
+
+
+handlers.sample = function(data, callback) {
+    // Callback a http status code and a payload object
+
+    callback(406, {'name' : 'sample handler'})
+};
+
+handlers.ping = function(data, callback) {
+    // Callback a http status code and a payload object
+
+    callback(200)
+};
+
+handlers.notFound = function(data, callback) {
+
+  callback(404);
+
+};
+
+
+// Define a request router
+var router = {
+  
+  'sample' : handlers.sample,
+  'ping' : handlers.ping
+
+}
